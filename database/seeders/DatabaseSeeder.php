5671ee1f239a8572d31afeaf13f4ddea6894a561
Naveed Hassan <08es34@gmail.com>
2024-09-12 19:17:39 +0500

    updated seeder functionality

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,39 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    private static int $numSeedersRan = 0;
+
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(NewsCategoriesSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        if (!static::$numSeedersRan) {
+            $this->command->info("Nothing to seed.");
+        }
+    }
+
+    public function call($class, $silent = false, $parameters = []): void
+    {
+        // check if the seeder already ran
+        $seeder = DB::table('seeders')->where('class', $class)->first();
+        if ($seeder) {
+            return;
+        }
+
+        // run the seeder
+        parent::call($class, $silent);
+
+        // add the seeder to the seeded list
+        DB::table('seeders')->insert([
+            'class' => $class,
+            'ran_at' => date('Y-m-d H:i:s'),
         ]);
+
+        static::$numSeedersRan++;
     }
 }
